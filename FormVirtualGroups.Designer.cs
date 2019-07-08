d453eafe5cbdcd77132a37fb8f983dfd0e659a86
romanio <rkositsyn@gmail.com>
2019-07-09 00:50:59 +0500

    add virtual load

--- FormVirtualGroups.Designer.cs
+++ FormVirtualGroups.Designer.cs
@@ -28,50 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.bbUpdate = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.bbLoadGroups = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.labelMin = new System.Windows.Forms.Label();
             this.listGroups = new System.Windows.Forms.ListBox();
+            this.listWells = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // bbUpdate
+            // bbLoadGroups
             // 
-            this.bbUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.bbUpdate.Location = new System.Drawing.Point(424, 18);
-            this.bbUpdate.Name = "bbUpdate";
-            this.bbUpdate.Size = new System.Drawing.Size(127, 29);
-            this.bbUpdate.TabIndex = 31;
-            this.bbUpdate.Text = "Load Groups";
-            this.bbUpdate.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(424, 53);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 29);
-            this.button1.TabIndex = 32;
-            this.button1.Text = "Save Groups";
-            this.button1.UseVisualStyleBackColor = true;
+            this.bbLoadGroups.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bbLoadGroups.Location = new System.Drawing.Point(12, 43);
+            this.bbLoadGroups.Name = "bbLoadGroups";
+            this.bbLoadGroups.Size = new System.Drawing.Size(127, 27);
+            this.bbLoadGroups.TabIndex = 31;
+            this.bbLoadGroups.Text = "Load Groups";
+            this.bbLoadGroups.UseVisualStyleBackColor = true;
+            this.bbLoadGroups.Click += new System.EventHandler(this.bbLoadGroups_Click);
             // 
             // button2
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.ForeColor = System.Drawing.Color.Red;
-            this.button2.Location = new System.Drawing.Point(424, 357);
+            this.button2.Location = new System.Drawing.Point(12, 134);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 29);
+            this.button2.Size = new System.Drawing.Size(127, 27);
             this.button2.TabIndex = 33;
             this.button2.Text = "Remove All Groups";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // labelMin
             // 
-            this.labelMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.labelMin.AutoSize = true;
-            this.labelMin.Location = new System.Drawing.Point(12, 18);
+            this.labelMin.Location = new System.Drawing.Point(162, 22);
             this.labelMin.Name = "labelMin";
             this.labelMin.Size = new System.Drawing.Size(55, 13);
             this.labelMin.TabIndex = 60;
@@ -79,24 +70,50 @@
             // 
             // listGroups
             // 
+            this.listGroups.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.listGroups.FormattingEnabled = true;
-            this.listGroups.HorizontalScrollbar = true;
             this.listGroups.IntegralHeight = false;
-            this.listGroups.Location = new System.Drawing.Point(15, 43);
+            this.listGroups.Location = new System.Drawing.Point(165, 43);
             this.listGroups.Name = "listGroups";
-            this.listGroups.Size = new System.Drawing.Size(111, 311);
+            this.listGroups.ScrollAlwaysVisible = true;
+            this.listGroups.Size = new System.Drawing.Size(120, 305);
+            this.listGroups.Sorted = true;
             this.listGroups.TabIndex = 61;
+            this.listGroups.SelectedIndexChanged += new System.EventHandler(this.listGroups_SelectedIndexChanged);
+            // 
+            // listWells
+            // 
+            this.listWells.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listWells.FormattingEnabled = true;
+            this.listWells.HorizontalScrollbar = true;
+            this.listWells.IntegralHeight = false;
+            this.listWells.Location = new System.Drawing.Point(307, 44);
+            this.listWells.Name = "listWells";
+            this.listWells.Size = new System.Drawing.Size(111, 305);
+            this.listWells.TabIndex = 62;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(304, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "Wells in Group";
             // 
             // FormVirtualGroups
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 456);
+            this.ClientSize = new System.Drawing.Size(432, 373);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listWells);
             this.Controls.Add(this.listGroups);
             this.Controls.Add(this.labelMin);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.bbUpdate);
+            this.Controls.Add(this.bbLoadGroups);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -111,10 +128,11 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button bbUpdate;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bbLoadGroups;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label labelMin;
         private System.Windows.Forms.ListBox listGroups;
+        private System.Windows.Forms.ListBox listWells;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FormVirtualGroups.cs
+++ FormVirtualGroups.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace mview
 {
@@ -19,9 +20,6 @@ namespace mview
         public FormVirtualGroups(EclipseProject ecl)
         {
             InitializeComponent();
-
-            boxMinimum.Text = ecl.FILENAME;
-            
             after_init = true;
         }
 
@@ -33,11 +31,6 @@ namespace mview
         private void boxMaximum_Validating(object sender, CancelEventArgs e)
         {
             double value;
-
-            if (double.TryParse(boxMaximum.Text, out value))
-            {
-                if (after_init) ApplyStyle();
-            }
         }
 
         private void boxMinimum_Validating(object sender, CancelEventArgs e)
@@ -45,11 +38,6 @@ namespace mview
             System.Diagnostics.Debug.WriteLine("BOX MINIMUM : VALIDATING");
 
             double value;
-
-            if (double.TryParse(boxMinimum.Text, out value))
-            {
-                if (after_init) ApplyStyle();
-            }
         }
 
         private void checkShowBubbles_CheckedChanged(object sender, EventArgs e)
@@ -73,6 +61,66 @@ namespace mview
         {
 
             if (after_init) ApplyStyle();
+        }
+
+        class VirtualGroupItem
+        {
+            public string wellname;
+            public string pad;
+        }
+
+        List<VirtualGroupItem> VirtualGroup = null;
+
+        private void bbLoadGroups_Click(object sender, EventArgs e)
+        {
+            var fd = new System.Windows.Forms.OpenFileDialog() { Filter = "User group file|*.csv" };
+
+            if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                VirtualGroup = new List<VirtualGroupItem>();
+
+                using (TextReader text = new StreamReader(fd.FileName, Encoding.GetEncoding("Windows-1251")))
+                {
+                    string line;
+
+                    while ((line = text.ReadLine()) != null)
+                    {
+                        string[] split = line.Split(new char[] { ';' });
+
+                        if (split.Length == 2)
+                        {
+                            VirtualGroup.Add(new VirtualGroupItem
+                            {
+                                wellname = split[0].Trim(),
+                                pad = split[1].Trim()
+                            });
+                        }
+                    }
+                    text.Close();
+
+                    listGroups.Items.Clear();
+
+                    var pads = (from item in VirtualGroup
+                                select item.pad).Distinct().ToArray();
+
+                    listGroups.Items.AddRange(pads);
+                }
+            }
+        }
+
+        private void listGroups_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listGroups.SelectedItem == null) return;
+
+            string selected_pad = listGroups.SelectedItem.ToString();
+
+            listWells.Items.Clear();
+
+            var wells = (from item in VirtualGroup
+                         where item.pad == selected_pad
+                         select item.wellname).ToArray();
+
+            listWells.Items.AddRange(wells);
         }
     }
 }
